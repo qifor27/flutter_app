e50277d056f99aeee16aba345648cb085276cffd
qifor27 <rofiqislamy88@gmail.com>
2026-02-28 18:26:31 +0700

    setup theme light and dark

---
+++ lib/utils/theme/theme.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'custom_themes/text_theme.dart';
+
+class RAppTheme {
+  RAppTheme._();
+
+  static ThemeData lightTheme = ThemeData(
+    useMaterial3: true,
+    fontFamily: 'Poppins',
+    brightness: Brightness.light,
+    primaryColor: Colors.blue,
+    scaffoldBackgroundColor: Colors.white,
+    textTheme: RTextTheme.lightTextTheme,
+    
+  );
+
+  static ThemeData darkTheme = ThemeData(
+    useMaterial3: true,
+    fontFamily: 'Poppins',
+    brightness: Brightness.dark,
+    primaryColor: Colors.blue,
+    scaffoldBackgroundColor: Colors.black,
+    textTheme: RTextTheme.darkTextTheme,
+  );
+}
